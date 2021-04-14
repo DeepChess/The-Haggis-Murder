f49bd911b169cf7c5970d0692d9a0bc3f120135e
lucas <lucas.brisonhenderick@edu.ece.fr>
2021-04-14 16:50:23 +0200

    choice level 4

--- external.js
+++ external.js
@@ -19,3 +19,314 @@ function toggle_text2(choice2) {
     document.getElementById("buttonChoice22").style.display= "inline";
   }
 }
+
+function toggle_text11(choice11) {
+  var span = document.getElementById(choice11);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice11").style.display= "none";
+    document.getElementById("buttonChoice12").style.display= "none";
+    document.getElementById("buttonChoice111").style.display= "inline";
+    document.getElementById("buttonChoice112").style.display= "inline";
+  }
+}
+
+function toggle_text12(choice12) {
+  var span = document.getElementById(choice12);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice11").style.display= "none";
+    document.getElementById("buttonChoice12").style.display= "none";
+    document.getElementById("buttonChoice121").style.display= "inline";
+    document.getElementById("buttonChoice122").style.display= "inline";
+  }
+}
+
+function toggle_text21(choice21) {
+  var span = document.getElementById(choice21);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice21").style.display= "none";
+    document.getElementById("buttonChoice22").style.display= "none";
+    document.getElementById("buttonChoice211").style.display= "inline";
+    document.getElementById("buttonChoice212").style.display= "inline";
+  }
+}
+
+function toggle_text22(choice22) {
+  var span = document.getElementById(choice22);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice21").style.display= "none";
+    document.getElementById("buttonChoice22").style.display= "none";
+    document.getElementById("buttonChoice221").style.display= "inline";
+    document.getElementById("buttonChoice222").style.display= "inline";
+  }
+}
+
+
+function toggle_text111(choice111) {
+  var span = document.getElementById(choice111);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice111").style.display= "none";
+    document.getElementById("buttonChoice112").style.display= "none";
+    document.getElementById("buttonChoice1111").style.display= "inline";
+    document.getElementById("buttonChoice1112").style.display= "inline";
+  }
+}
+
+function toggle_text112(choice112) {
+  var span = document.getElementById(choice112);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice111").style.display= "none";
+    document.getElementById("buttonChoice112").style.display= "none";
+    document.getElementById("buttonChoice1121").style.display= "inline";
+    document.getElementById("buttonChoice1122").style.display= "inline";
+  }
+}
+
+function toggle_text121(choice121) {
+  var span = document.getElementById(choice121);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice121").style.display= "none";
+    document.getElementById("buttonChoice122").style.display= "none";
+    document.getElementById("buttonChoice1211").style.display= "inline";
+    document.getElementById("buttonChoice1212").style.display= "inline";
+  }
+}
+
+function toggle_text122(choice122) {
+  var span = document.getElementById(choice122);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice121").style.display= "none";
+    document.getElementById("buttonChoice122").style.display= "none";
+    document.getElementById("buttonChoice1221").style.display= "inline";
+    document.getElementById("buttonChoice1222").style.display= "inline";
+  }
+}
+
+function toggle_text211(choice211) {
+  var span = document.getElementById(choice211);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice211").style.display= "none";
+    document.getElementById("buttonChoice212").style.display= "none";
+    document.getElementById("buttonChoice2111").style.display= "inline";
+    document.getElementById("buttonChoice2112").style.display= "inline";
+  }
+}
+
+function toggle_text212(choice212) {
+  var span = document.getElementById(choice212);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice211").style.display= "none";
+    document.getElementById("buttonChoice212").style.display= "none";
+    document.getElementById("buttonChoice2121").style.display= "inline";
+    document.getElementById("buttonChoice2122").style.display= "inline";
+  }
+}
+
+function toggle_text221(choice221) {
+  var span = document.getElementById(choice221);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice221").style.display= "none";
+    document.getElementById("buttonChoice222").style.display= "none";
+    document.getElementById("buttonChoice2211").style.display= "inline";
+    document.getElementById("buttonChoice2212").style.display= "inline";
+  }
+}
+
+function toggle_text222(choice222) {
+  var span = document.getElementById(choice222);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice221").style.display= "none";
+    document.getElementById("buttonChoice222").style.display= "none";
+    document.getElementById("buttonChoice2221").style.display= "inline";
+    document.getElementById("buttonChoice2222").style.display= "inline";
+  }
+}
+
+
+function toggle_text1111(choice1111) {
+  var span = document.getElementById(choice1111);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1111").style.display= "none";
+    document.getElementById("buttonChoice1112").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text12(choice1112) {
+  var span = document.getElementById(choice1112);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1111").style.display= "none";
+    document.getElementById("buttonChoice1112").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text1121(choice1121) {
+  var span = document.getElementById(choice1121);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1121").style.display= "none";
+    document.getElementById("buttonChoice1122").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+function toggle_text1122(choice1122) {
+  var span = document.getElementById(choice1122);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1121").style.display= "none";
+    document.getElementById("buttonChoice1122").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+function toggle_text1211(choice1211) {
+  var span = document.getElementById(choice1211);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1211").style.display= "none";
+    document.getElementById("buttonChoice1212").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text1212(choice1212) {
+  var span = document.getElementById(choice1212);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1211").style.display= "none";
+    document.getElementById("buttonChoice1212").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text1221(choice1221) {
+  var span = document.getElementById(choice1221);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1221").style.display= "none";
+    document.getElementById("buttonChoice1222").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+function toggle_text1222(choice1222) {
+  var span = document.getElementById(choice1222);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice1221").style.display= "none";
+    document.getElementById("buttonChoice1222").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+
+function toggle_text2111(choice2111) {
+  var span = document.getElementById(choice2111);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2111").style.display= "none";
+    document.getElementById("buttonChoice2112").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text2112(choice2112) {
+  var span = document.getElementById(choice2112);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2111").style.display= "none";
+    document.getElementById("buttonChoice2112").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text2121(choice2121) {
+  var span = document.getElementById(choice2121);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2121").style.display= "none";
+    document.getElementById("buttonChoice2122").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+function toggle_text2122(choice2122) {
+  var span = document.getElementById(choice2122);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2121").style.display= "none";
+    document.getElementById("buttonChoice2122").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+function toggle_text2211(choice2211) {
+  var span = document.getElementById(choice2211);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2211").style.display= "none";
+    document.getElementById("buttonChoice2212").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text2212(choice2212) {
+  var span = document.getElementById(choice2212);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2211").style.display= "none";
+    document.getElementById("buttonChoice2212").style.display= "none";
+  ///  document.getElementById("buttonChoice11").style.display= "inline";
+  ///  document.getElementById("buttonChoice12").style.display= "inline";
+  }
+}
+
+function toggle_text2221(choice2221) {
+  var span = document.getElementById(choice2221);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2221").style.display= "none";
+    document.getElementById("buttonChoice2222").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
+
+function toggle_text2222(choice2222) {
+  var span = document.getElementById(choice2222);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+    document.getElementById("buttonChoice2221").style.display= "none";
+    document.getElementById("buttonChoice2222").style.display= "none";
+  ///  document.getElementById("buttonChoice21").style.display= "inline";
+  ///  document.getElementById("buttonChoice22").style.display= "inline";
+  }
+}
